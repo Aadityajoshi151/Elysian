a938c3bb21cac4fc6109507e377c551d6a773972
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-13 19:07:35 +0530

    adds dotenv package to read from .env file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const routes = require('./routes');
 
 app.use(express.json)
 app.use('/api', routes);
-  
+
 const PORT = process.env.PORT || 6161;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
