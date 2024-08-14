b0a21d9c5b13c09e28f1563eb5e100730174bd69
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-14 09:51:10 +0530

    creates and calls skeleton code for createBookmarksFile function

--- src/controllers/exportToElysianController.js
+++ src/controllers/exportToElysianController.js
@@ -1,5 +1,8 @@
+const writeFile = require('../utils/writeFile');
+
 const handleExportToElysian = (req, res) => {
     // Logic for handling export
+    writeFile.createBookmarksFile()
     res.json({ message: 'Export successful' });
 };
 
---
+++ src/utils/writeFile.js
@@ -0,0 +1,9 @@
+const createBookmarksFile = () => {
+    //Logic for creating bookmarks.json file
+    console.log("Writing file!!")
+
+}
+
+module.exports = {
+    createBookmarksFile
+};
