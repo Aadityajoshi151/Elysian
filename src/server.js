ed5ef7284c3677c6eccd8f6e3c3904ff319d46f8
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-14 09:41:48 +0530

    dummy export_to_elysian route returns dummy response

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config()
 const routes = require('./routes');
 
-app.use(express.json)
+//app.use(express.json)
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 6161;
