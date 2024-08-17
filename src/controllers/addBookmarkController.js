081e227504a8508eb7e6537e9cdce5909583d3f5
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-17 16:56:45 +0530

    adds logic to add bookmark at the end of the bookmarks.json file

--- src/controllers/addBookmarkController.js
+++ src/controllers/addBookmarkController.js
@@ -1,4 +1,13 @@
+const readFile = require('../utils/readFile');
+const writeFile = require('../utils/writeFile');
+
 const handleAddBookmark = (req, res) => {
+    bookmarks = readFile.readBookmarksFile()
+    bookmarks = JSON.parse(bookmarks)
+    if (req.body.parentId === '1'){
+        bookmarks.push(req.body)
+    }
+    writeFile.createBookmarksFile(JSON.stringify(bookmarks))
     res.json({ message: 'Bookmark Added' });
 };
 
