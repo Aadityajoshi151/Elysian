44717bcd0e6005242a19102bb005ebe7c673a1e5
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-20 09:10:54 +0530

    adds better logic to find and delete the node

--- src/controllers/deleteBookmarkController.js
+++ src/controllers/deleteBookmarkController.js
@@ -3,14 +3,10 @@ const writeFile = require('../utils/writeFile');
 
 const handleDeleteBookmark = (req, res) => {
     bookmarks = readFile.readBookmarksFile()
-    bookmarks = JSON.parse(bookmarks)
-    for (let i = 0; i < bookmarks.length; i++) {
-        let node = bookmarks[i];
-        if (node.id === req.body.id){
-           console.log("To be deleted:")
-           console.log(node)
-        }
-    }  
+    bookmarks = JSON.parse(bookmarks)   
+    const indexToRemove = bookmarks.findIndex((bookmarks) => bookmarks.id === req.body.id);
+    bookmarks.splice(indexToRemove, 1)
+    writeFile.createBookmarksFile(JSON.stringify(bookmarks))
     res.json({ message: 'Bookmark Deleted' });
 };
 
