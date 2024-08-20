a85373fe5d907d183da96b26a54732c6ddee6802
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-20 08:46:03 +0530

    adds logic to find the node to be deleted

--- src/controllers/deleteBookmarkController.js
+++ src/controllers/deleteBookmarkController.js
@@ -2,7 +2,15 @@ const readFile = require('../utils/readFile');
 const writeFile = require('../utils/writeFile');
 
 const handleDeleteBookmark = (req, res) => {
-    console.log("bookmark deleted")
+    bookmarks = readFile.readBookmarksFile()
+    bookmarks = JSON.parse(bookmarks)
+    for (let i = 0; i < bookmarks.length; i++) {
+        let node = bookmarks[i];
+        if (node.id === req.body.id){
+           console.log("To be deleted:")
+           console.log(node)
+        }
+    }  
     res.json({ message: 'Bookmark Deleted' });
 };
 
