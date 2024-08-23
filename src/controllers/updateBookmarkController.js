21e3b1f2fc3dd78358725bd30118bd1f06eb40fa
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-23 12:43:29 +0530

    adds function to recursively update any node

--- src/controllers/updateBookmarkController.js
+++ src/controllers/updateBookmarkController.js
@@ -1,12 +1,29 @@
 const readFile = require('../utils/readFile');
 const writeFile = require('../utils/writeFile');
 
+function updateChildBookmark(bookmarks, new_bookmark){
+    //TODO Add return statements during error handling
+    for (let i = 0; i < bookmarks.length; i++) {
+        let node = bookmarks[i];
+        if (node.id === new_bookmark.id) {
+            if (new_bookmark.url === undefined){  //folder is updated
+                node.title = new_bookmark.title
+            }
+            else{ //bookmark is updated
+                node.title = new_bookmark.title;
+                node.url = new_bookmark.url;
+            }
+        }
+        if (node.children && node.children.length > 0) {
+            updateChildBookmark(node.children, new_bookmark);
+        }
+    }
+}
+
 const handleUpdateBookmark = (req, res) => {
     bookmarks = readFile.readBookmarksFile()
-    bookmarks = JSON.parse(bookmarks)   
-    const indexToUpdate = bookmarks.findIndex((bookmarks) => bookmarks.id === req.body.id);
-    bookmarks[indexToUpdate].title = req.body.title
-    bookmarks[indexToUpdate].url = req.body.url
+    bookmarks = JSON.parse(bookmarks)
+    updateChildBookmark(bookmarks, req.body)
     writeFile.createBookmarksFile(JSON.stringify(bookmarks))
     res.json({ message: 'Bookmark Updated' });
 };
