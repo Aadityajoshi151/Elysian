3a2545e71c0372a3669a2ee684d8769509f75059
Aaditya Joshi <aadityajoshi151@gmail.com>
2024-08-23 17:41:02 +0530

    🐛 fixes broken logic for recursive bookmark deletion (#29)

--- src/controllers/deleteBookmarkController.js
+++ src/controllers/deleteBookmarkController.js
@@ -1,12 +1,29 @@
 const readFile = require('../utils/readFile');
 const writeFile = require('../utils/writeFile');
 
+function deleteChildNode(bookmarks, id_to_be_deleted) {
+    for (let i = 0; i < bookmarks.length; i++) {
+        const node = bookmarks[i];
+        if (node.id === id_to_be_deleted) {
+            bookmarks.splice(i, 1);
+            i--;
+            continue;
+        }
+        if (node.children && node.children.length > 0) {
+            node.children = deleteChildNode(node.children, id_to_be_deleted);
+        }
+    }
+    return bookmarks;
+}
+
+
 const handleDeleteBookmark = (req, res) => {
     bookmarks = readFile.readBookmarksFile()
-    bookmarks = JSON.parse(bookmarks)   
-    const indexToRemove = bookmarks.findIndex((bookmarks) => bookmarks.id === req.body.id);
-    bookmarks.splice(indexToRemove, 1)
-    writeFile.createBookmarksFile(JSON.stringify(bookmarks))
+    bookmarks = JSON.parse(bookmarks)
+    //TODO Add error handling
+    //result will be true or false if bookmark deleted or not
+    result = deleteChildNode(bookmarks, req.body.id)
+    writeFile.createBookmarksFile(JSON.stringify(result))
     res.json({ message: 'Bookmark Deleted' });
 };
 
