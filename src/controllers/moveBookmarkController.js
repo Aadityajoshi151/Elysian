6d96ed2d3d92cd694d3b14aa17d1ebf66e762b61
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-24 17:15:40 +0530

    🎨 adds logic to export all bookmarks when one is moved

--- src/controllers/moveBookmarkController.js
+++ src/controllers/moveBookmarkController.js
@@ -1,11 +1,7 @@
-const readFile = require('../utils/readFile');
 const writeFile = require('../utils/writeFile');
 
 const handleMoveBookmark = (req, res) => {
-    //bookmarks = readFile.readBookmarksFile()
-    //bookmarks = JSON.parse(bookmarks)
-    console.log("move bookmark")
-    //writeFile.createBookmarksFile(JSON.stringify(result))
+    writeFile.createBookmarksFile(JSON.stringify(req.body))
     res.json({ message: 'Bookmark Moved' });
 };
 
