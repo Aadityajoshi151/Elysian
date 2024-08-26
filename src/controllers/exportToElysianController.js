e1853273c9e49788f72e76d44dc2db9e983aa2e8
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-08-26 09:14:12 +0530

    🎨 send response as json

--- src/controllers/exportToElysianController.js
+++ src/controllers/exportToElysianController.js
@@ -4,10 +4,10 @@ const authCheck = require('../utils/authCheck');
 const handleExportToElysian = (req, res) => {
     if (authCheck.isAuthorized(req.headers.authorization)){
         writeFile.createBookmarksFile(JSON.stringify(req.body))
-        res.status(200).send('Export successful');
+        res.status(200).json('Export successful');
     }
     else{
-        res.status(403).send('Unauthorized');
+        res.status(403).json('Unauthorized');
     }
 
 };
