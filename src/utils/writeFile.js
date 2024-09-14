4cb4405fb488b00c6d93df4163bf44015a7bb665
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-09-14 10:58:47 +0530

    🥅 adds error handling and logging to writeFIle

--- src/utils/writeFile.js
+++ src/utils/writeFile.js
@@ -2,9 +2,14 @@ const path = require('path')
 const fs = require('fs');
 
 const createBookmarksFile = (bookmarks) => {
-    const filePath = path.join(__dirname, '../data/bookmarks.json');
-    fs.writeFileSync(filePath, bookmarks, 'utf-8');
-    console.log(`Bookmarks.json have been saved to ${filePath}`);
+    try {
+        const filePath = path.join(__dirname, '../data/bookmarks.json');
+        fs.writeFileSync(filePath, bookmarks, 'utf-8');
+        console.log(createBookmarksFile.name + ': Bookmarks are saved to ' + filePath);
+    }
+    catch (err) {
+        console.error(createBookmarksFile.name + ': ' + err)
+    }
 }
 
 module.exports = {
