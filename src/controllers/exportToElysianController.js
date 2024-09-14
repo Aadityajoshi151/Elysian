1bde956bd3abb70cb08e66fa85aab66f2241a1c4
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-09-14 11:06:04 +0530

    🥅 adds error handling and logging to export function

--- src/controllers/exportToElysianController.js
+++ src/controllers/exportToElysianController.js
@@ -2,12 +2,20 @@ const writeFile = require('../utils/writeFile');
 const authCheck = require('../utils/authCheck');
 
 const handleExportToElysian = (req, res) => {
-    if (authCheck.isAuthorized(req.headers.authorization)){
-        writeFile.createBookmarksFile(JSON.stringify(req.body))
-        res.status(200).json('Export successful');
+    try {
+        if (authCheck.isAuthorized(req.headers.authorization)) {
+            writeFile.createBookmarksFile(JSON.stringify(req.body))
+            res.status(200).json('Export successful');
+            console.log(handleExportToElysian.name + ': Export successful')
+
+        }
+        else {    
+            res.status(401).json('Unauthorized');
+            console.error(handleExportToElysian.name + ': Unauthorized')
+        }
     }
-    else{
-        res.status(401).json('Unauthorized');
+    catch (err) {
+        console.error(handleExportToElysian.name+': '+err)
     }
 };
 
